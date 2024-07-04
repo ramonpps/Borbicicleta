1f4bd765604c279ac1fbed7b6b11ab0e399c8bf7
GabrielGomes191 <antoniocgoncalves53@gmail.com>
2024-07-04 19:52:18 -0300

    feat: Tarefa 2 feita

---
+++ TratamentoViagens.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import re
+
+class DataCleaner:
+    @staticmethod
+    def starts_with_number(s):
+        return str(s)[0].isdigit() if pd.notnull(s) else False
+
+    @staticmethod
+    def extrair_primeiros_numeros(s):
+        if "Depois das" in s:
+            return s
+        match = re.findall(r'\d+', str(s))
+        return match[0] if match else s
+
+    def __init__(self, csvOrigem):
+        self.csvOrigem = csvOrigem
+        self.df = pd.read_csv(csvOrigem)
+
+    def selecionar_colunas_validas(self):
+        colunas_validas = [col for col in self.df.columns if DataCleaner.starts_with_number(col)]
+        self.df = self.df[colunas_validas]
+
+    def salvar_csv_tratado(self, output_path='outputs\Mobilidade_Eletrica_Niterói_Tratado.csv'):
+        self.df.to_csv(output_path, index=False)
+
+class DataFormatter:
+    def __init__(self, csvOrigem):
+        self.csvOrigem = csvOrigem
+        self.df = pd.read_csv(csvOrigem)
+        self.dados_formatados = []
+
+    def formatar_dados(self):
+        for index, row in self.df.iterrows():
+            for i in range(0, len(row), 3):
+                if i + 2 < len(row) and pd.notnull(row[i]) and pd.notnull(row[i + 1]) and pd.notnull(row[i + 2]):
+                    hora = DataCleaner.extrair_primeiros_numeros(row[i])
+                    origem = row[i + 1]
+                    destino = row[i + 2]
+                    self.dados_formatados.append([hora, origem, destino])
+
+    def criar_novo_dataframe(self):
+        novo_df = pd.DataFrame(self.dados_formatados, columns=['SAÍDA', 'ORIGEM', 'DESTINO'])
+        novo_df.dropna(inplace=True)
+        novo_df.insert(0, 'VIAGEM', range(1, len(novo_df) + 1))
+        return novo_df
+
+    def salvar_csv_formatado(self, output_path='outputs\Relação_Viagens.csv'):
+        novo_df = self.criar_novo_dataframe()
+        novo_df.to_csv(output_path, index=False)
+
+class DataProcessor:
+    @staticmethod
+    def processar_dados():
+        cleaner = DataCleaner("inputs\Mobilidade Elétrica Niterói.csv")
+        cleaner.selecionar_colunas_validas()
+        cleaner.salvar_csv_tratado()
+
+        formatter = DataFormatter('outputs\Mobilidade_Eletrica_Niterói_Tratado.csv')
+        formatter.formatar_dados()
+        formatter.salvar_csv_formatado()
+
+# Executa o processamento dos dados
+DataProcessor.processar_dados()
